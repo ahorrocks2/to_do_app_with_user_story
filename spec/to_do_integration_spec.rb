0e86d87e325f81797fc2f4356d4ac2f3ff1fb822
Kendra Ash & Alyssa Horrocks <student@epicodus.com>
2015-08-18 10:45:11 -0700

    Allow user to add a new list to their to do lists page.

--- spec/to_do_integration_spec.rb
+++ spec/to_do_integration_spec.rb
@@ -6,13 +6,13 @@ set(:show_exceptions, false)
 describe('the list path', {:type => :feature}) do
   it('allows the user to see all lists') do
     visit('/')
-    click_button('View all lists')
+    click_on('View all lists')
     expect(page).to have_content('View All Lists')
   end
 
   it('allow the user to add a new list') do
     visit('/')
-    click_button('Add a list')
+    click_on('Add a list')
     expect(page).to have_content('New List:')
   end
 
